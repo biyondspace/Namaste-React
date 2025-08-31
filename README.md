9855ccd00d06dc6fb4b33d61ffe487e555ce3f21
kumkum vishvakarma <vkumkum200@gmail.com>
2025-08-31 12:38:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Namaste React
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot module Replacement
+- File wathcing algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- compress
+- Consistent hashing
+- code splitting
+- Differential Bundling - support older browser
+- Diagnostic
+- Error handling
+- HTTPS
+- tree shaking = remove unused code
+- Different dev and prod bundles
+
+# Namaste Food
+
+-Header
+-logo
+-nav items like Home, About
+
+- Body
+  -Search bar
+  -Restaurant container
+  -restaurant card
+  -Img
+  -Name of res, Star Rating, cuisine, Delivery time
+- Footer
+  -copyright
+  -links
+
+* Address
+* contact
---
+++ src/components/Body.js
@@ -0,0 +1,84 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendered");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.19810&lng=72.82980&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurant.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res?.info?.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
