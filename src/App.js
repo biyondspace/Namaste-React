e10d5b10d9a43202592dff22cddeafe1d12fc701
kumkum vishvakarma <vkumkum200@gmail.com>
2025-09-05 12:20:31 +0530

    Third commit: updated router setup and package.json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Applayout = () => {
   return (
@@ -12,5 +14,16 @@ const Applayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Applayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout />);
+root.render(<RouterProvider router={appRouter} />);
